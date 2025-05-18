ef55fd4f80d9cde75ae6cca7fbddb2d84c2e87e8
Joao Paulo Schwarz Schuler <jp@schulers.com>
2025-05-17 21:16:13 -0300

    Adding bp_parse_code_blobs

--- src/smolagents/bp_utils.py
+++ src/smolagents/bp_utils.py
@@ -1,4 +1,7 @@
 import time
+import ast
+import re
+from textwrap import dedent
 
 def delay_execution_10(pagent, **kwargs) -> bool:
     """
@@ -20,3 +23,42 @@ def delay_execution_120(pagent, **kwargs) -> bool:
     """
     time.sleep(120)
     return True
+
+def bp_parse_code_blobs(text: str) -> str:
+    """Extract code blocs from the LLM's output.
+
+    If a valid code block is passed, it returns it directly.
+
+    Args:
+        text (`str`): LLM's output text to parse.
+
+    Returns:
+        `str`: Extracted code block.
+
+    Raises:
+        ValueError: If no valid code block is found in the text.
+    """
+    pattern = r"```(?:runpy)\s*\n(.*?)\n```"
+    matches = re.findall(pattern, text, re.DOTALL)
+    if matches:
+        return "\n\n".join(match.strip() for match in matches)
+    # Maybe the LLM outputted a code blob directly
+    try:
+        ast.parse(text)
+        return text
+    except SyntaxError:
+        raise ValueError(
+            dedent(
+                f"""
+Your code snippet is invalid, because the regex pattern {pattern} was not found in it.
+Here is your code snippet:
+{text}
+Make sure to include code with the correct pattern, for instance:
+Thoughts: Your thoughts
+Code:
+```runpy
+# Your python code here
+```<end_code>
+"""
+            ).strip()
+        )
